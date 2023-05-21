40606f4adbe04423b37ad77b4b661492996afd3f
Mustakim <sycospike1@gmail.com>
2023-05-21 15:10:02 +0600

    added category's id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,6 +32,16 @@ async function run() {
     app.get('/categories', async(req, res) => {
       const cursor = toyCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/categories/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const options = {
+        projection: {name:1,category:1,price:1,rating:1}
+      };
+      const result = await toyCollection.findOne(query, options);
       res.send(result);
     })
 
